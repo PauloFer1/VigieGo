e8f20179adf622f1308f4c33a24011d636f4dc29
MacBook13 <paulo.r.r.fernandes@gmail.com>
2016-06-17 00:03:08 +0100

    UnitTest testing

---
+++ app/src/test/java/com/tarambola/view/ChartTest.java
@@ -0,0 +1,24 @@
+package com.tarambola.view;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by macbook13 on 16/06/16.
+ */
+public class ChartTest {
+
+    @Test
+    public void chartCreateTest() throws Exception
+    {
+        Chart chart = new Chart();
+
+
+    }
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
