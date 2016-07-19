91d7f1772b26108d878080c3ceef70797c2aff42
MacBook13 <paulo.r.r.fernandes@gmail.com>
2016-07-19 09:38:45 +0100

    Started Presenter Layer

---
+++ app/src/androidTest/java/com/tarambola/view/MainActivityTest_OLD.java
@@ -0,0 +1,17 @@
+package com.tarambola.view;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by Paulo on 08/06/2016.
+ */
+public class MainActivityTest_OLD extends TestCase {
+
+    public void testOnCreate() throws Exception {
+
+    }
+
+    public void testOnNewIntent() throws Exception {
+
+    }
+}
